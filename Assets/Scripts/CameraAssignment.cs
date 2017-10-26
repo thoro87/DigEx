4764c14bdc60a563705b1dcedfc17569609dc769
Thoro87 <thorondors@gmx.de>
2017-10-26 12:16:01 +0200

    Updated Surveillance Scene
    Added 2 Modes:
    1. Random Camera Assignment
    2. Rotating Rop Row

---
+++ Assets/Scripts/CameraAssignment.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAssignment : MonoBehaviour {
+
+    public Transform TopRow;
+
+    private int Mode = 1;
+    private float Interval;
+    private float Timer;
+    private Camera[] CamerasTopRow;
+    private Camera[] CamerasAll;
+    private Camera[] CurrentCameras;
+    private int CurrentIndex;
+
+	void Start () {
+        CamerasTopRow = TopRow.GetComponentsInChildren<Camera>();
+        CamerasAll = GetComponentsInChildren<Camera>();
+        CurrentCameras = CamerasAll;
+        SetMode(Mode);
+	}
+	
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer >= Interval) {
+            Timer = 0f;
+
+            CurrentCameras[CurrentIndex].enabled = false;
+
+            if (Mode == 1) {
+                // 1: Random all
+                CurrentIndex = Random.Range(0, CamerasAll.Length);
+            } else if (Mode == 2) {
+                // 2: TopRow Circle
+                CurrentIndex++;
+                if (CurrentIndex == CamerasTopRow.Length) {
+                    CurrentIndex = 0;
+                }
+            }
+            CurrentCameras[CurrentIndex].enabled = true;
+        }
+	}
+
+    public void SetMode(int newMode) {
+        CurrentCameras[CurrentIndex].enabled = false;
+
+        Mode = newMode;
+        Timer = 0f;
+        if (newMode == 1) {
+            // 1: Random all
+            Interval = 1f;
+            CurrentIndex = Random.Range(0, CamerasAll.Length);
+            CurrentCameras = CamerasAll;
+        } else if (newMode == 2) {
+            // 2: TopRow cirlce
+            Interval = 0.05f;
+            CurrentIndex = 0;
+            CurrentCameras = CamerasTopRow;
+        }
+        CurrentCameras[CurrentIndex].enabled = true;
+    }
+}
--- Assets/Scripts/IngameMenu.cs
+++ Assets/Scripts/IngameMenu.cs
@@ -4,9 +4,31 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Rendering;
 
 public class IngameMenu : MonoBehaviour {
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.Escape)) {
+
+    public GameObject Group_Surveillance;
+
+    private Scene CurrentScene;
+
+    void Start() {
+        CurrentScene = SceneManager.GetActiveScene();
+        Group_Surveillance.SetActive(CurrentScene.name == "surveillance");
+    }
+
+    void Update() {
+
+        if (CurrentScene.name == "surveillance") {
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
+                GameObject.Find("SimpleCameras").GetComponent<CameraAssignment>().SetMode(1);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) {
+                GameObject.Find("SimpleCameras").GetComponent<CameraAssignment>().SetMode(2);
+            }
+        }
+
+        #if !UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Escape)) {
 			SceneManager.LoadScene ("menu");
 		}
-	}
+        #endif
+    }
 }
