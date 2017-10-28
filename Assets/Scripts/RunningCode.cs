b99d54b6d610bece8f793dd5e6bf359448c795c5
Thoro87 <thorondors@gmx.de>
2017-10-28 13:47:10 +0200

    Text room
    added text room

---
+++ Assets/Scripts/RunningCode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class RunningCode : MonoBehaviour {
+
+    public TextAsset CodeFile;
+
+    private TextMesh TextMesh;
+    private string[] Lines;
+    private int CurrentRow = 0;
+    private int TotalRows = 26;
+    private float Timer;
+    private float Duration = 0.1f;
+
+	void Start () {
+        TextMesh = GetComponent<TextMesh>();
+        Lines = Regex.Split(CodeFile.text, "\n");
+        UpdateText();
+	}
+	
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer >= Duration) {
+            Timer = 0;
+            if (UnityEngine.Random.Range(1, 100) > 92) {
+                // only a few slow ones
+                Duration = UnityEngine.Random.Range(0.3f, 0.6f);
+            } else {
+                Duration = UnityEngine.Random.Range(0.05f, 0.2f);
+            }
+            UpdateText();
+            CurrentRow++;
+            if (CurrentRow == Lines.Length) {
+                CurrentRow = 0;
+            }
+        }
+	}
+
+    private void UpdateText() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < TotalRows; i++) {
+            int row = CurrentRow + i;
+            if (row >= Lines.Length) {
+                row -= Lines.Length;
+            }
+            builder.AppendLine(Lines[row]);
+        }
+        TextMesh.text = builder.ToString();
+    }
+}
---
+++ Assets/Scripts/ScrollText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollText : MonoBehaviour {
+
+    public float Duration = 20f;
+
+    private float Start = 27f;
+    private float End = -117f;
+
+    private float Timer;
+
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer > Duration) {
+            Timer = 0;
+        }
+        Vector3 newPos = transform.position;
+        newPos.z = Mathf.Lerp(Start, End, Timer / Duration);
+        transform.position = newPos;
+	}
+}
