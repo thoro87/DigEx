2c85fae9a5e82ab3d4d4bbf8ff23cde60ecbcaa9
Thoro87 <thorondors@gmx.de>
2017-10-28 09:58:51 +0200

    FOV Scene
    - added fov scene
    - reduced size of 1920s video

---
+++ Assets/Scripts/FovManipulation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FovManipulation : MonoBehaviour {
+
+    private Camera Camera;
+    private Transform PlayerTrans;
+
+	void Start () {
+        Camera = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+        PlayerTrans = GameObject.Find("Player").transform;
+	}
+	
+	void Update () {
+        float t = PlayerTrans.position.z / 30f;
+        float newFov = Mathf.Lerp(30, 100f, t);
+        Camera.fieldOfView = newFov;
+	}
+}
