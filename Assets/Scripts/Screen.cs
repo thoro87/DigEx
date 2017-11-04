8ea7ba2c4c1ebd8ea3b888206ff1251fa0cef9ef
Thoro87 <thorondors@gmx.de>
2017-11-04 09:24:23 +0100

    Back to MovieTexture
    The goal is not to do everything in the browser, so I switched back from
    WebGL to MovieTextures for regular builds

--- Assets/Scripts/Screen.cs
+++ Assets/Scripts/Screen.cs
@@ -3,40 +3,23 @@ using System.Collections;
 
 public class Screen : MonoBehaviour {
 
-    WebGLMovieTexture tex;
-    bool videoRunning;
+    //WebGLMovieTexture tex;
+    //bool videoRunning;
 
     void Start() {
-        tex = new WebGLMovieTexture("StreamingAssets/The Roaring 1920s.mp4");
-        GetComponent<MeshRenderer>().material.mainTexture = tex;
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+
+        //tex = new WebGLMovieTexture("StreamingAssets/The Roaring 1920s.mp4");
+        //GetComponent<MeshRenderer>().material.mainTexture = tex;
     }
 
     void Update() {
-        if (tex.isReady && !videoRunning) {
-            tex.Play();
-            tex.loop = true;
-            videoRunning = true;
-        }
-        tex.Update();
+
+        //if (tex.isReady && !videoRunning) {
+        //    tex.Play();
+        //    tex.loop = true;
+        //    videoRunning = true;
+        //}
+        //tex.Update();
     }
-
-    //void OnGUI() {
-    //    GUI.enabled = tex.isReady;
-
-    //    GUILayout.BeginHorizontal();
-    //    if (GUILayout.Button("Play"))
-    //        tex.Play();
-    //    if (GUILayout.Button("Pause"))
-    //        tex.Pause();
-    //    tex.loop = GUILayout.Toggle(tex.loop, "Loop");
-    //    GUILayout.EndHorizontal();
-
-    //    var oldT = tex.time;
-    //    var newT = GUILayout.HorizontalSlider(tex.time, 0.0f, tex.duration);
-    //    if (!Mathf.Approximately(oldT, newT)) {
-    //        tex.Seek(newT);
-    //    }
-
-    //    GUI.enabled = true;
-    //}
 }
