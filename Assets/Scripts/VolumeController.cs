eeaa9b3fd0fe94b159d214da8cda536b1360145a
Thoro87 <thorondors@gmx.de>
2017-12-10 10:40:59 +0100

    Hallway
    - started hallway

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    public float Z_1;
+    public float Z_0;
+
+    private AudioSource AudioSource;
+
+	void Start () {
+        AudioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        float vol;
+        if (PlayerTransform.position.z < Z_1) {
+            vol = 1;
+        } else if (PlayerTransform.position.z > Z_0) {
+            vol = 0;
+        } else {
+            float dist = PlayerTransform.position.z - Z_1;
+
+            vol = 1 - (dist / (Z_0 - Z_1));
+        }
+        AudioSource.volume = vol;
+	}
+}
